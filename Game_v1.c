c6bd32b43be7d98b4b8d8c9a7c2d9903658991bb
ArnauArevaloMorell <167523871+ArnauArevaloMorell@users.noreply.github.com>
2024-05-01 17:32:27 +0200

    Add files via upload

---
+++ Game_v1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Define the structs
+typedef struct {
+    char name[50];
+    int HP;
+    int atk_pts;
+    int def_pts;
+    int skills[4];
+} Character;
+
+typedef struct {
+    char name[50];
+    char description[100];
+    bool type;
+    int duration;
+    int mod_atk;
+    int mod_def;
+    int mod_hp;
+} Skill;
+
+typedef struct {
+    char name[50];
+    int atk;
+    int hp;
+    int def;
+}Enemy;
+
+typedef struct{
+    char response_text[100];
+    char narrative_text_i[200];
+    Enemy enemies[3];
+    char narrative_text_f[200];
+}Option;
+
+typedef struct {
+    char question_text[200];
+    int number_options;
+    Option *what_happens;
+}Decision;
+
+typedef struct {
+    char name[50];
+    char description[200];
+    Decision *choice;
+}Scenario;
+
+
+int main(){
+    printf("Welcome! This is an RPG in which the player delves into the everyday life of an Investment Banker, where his ultimate goal is to be richer than the all-powerful Blackstone");
+    return 0;
+}
