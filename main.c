8672eac0e46a96fb8e9482a8115c08f477ff8c91
arblot8 <167622595+arblot8@users.noreply.github.com>
2024-05-01 17:36:08 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <structures.h>
+
+
+int main(){
+    printf("Welcome! This is an RPG in which the player delves into the everyday life of an Investment Banker, where his ultimate goal is to be richer than the all-powerful Blackstone");
+    return 0;
+}
